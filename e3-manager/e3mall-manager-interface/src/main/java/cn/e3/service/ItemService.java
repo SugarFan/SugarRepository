53a3560e98b82ea4f22d864b4534da5fba6aa7cb
guest1 <guest1@Sugar>
2018-01-09 17:11:41 +0800

    第二天e3-manager service成为了一个应用war包， 添加了interface

--- e3-manager/e3mall-manager-interface/src/main/java/cn/e3/service/ItemService.java
+++ e3-manager/e3mall-manager-interface/src/main/java/cn/e3/service/ItemService.java
@@ -1,7 +1,11 @@
 package cn.e3.service;
 
+import cn.e3.pojo.EasyUIPageBean;
 import cn.e3.pojo.TbItem;
 
 public interface ItemService {
 	public TbItem findItemById(Long itemId);
+	
+	//查询商品列表
+	public EasyUIPageBean itemList(Integer page,Integer rows);
 }
--- e3-manager/e3mall-manager-service/src/main/java/cn/e3/service/ItemServiceImpl.java
+++ e3-manager/e3mall-manager-service/src/main/java/cn/e3/service/ItemServiceImpl.java
@@ -2,14 +2,16 @@ package cn.e3.service;
 
 import java.util.List;
 
-import org.apache.commons.lang3.ObjectUtils.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+
 import cn.e3.mapper.TbItemMapper;
+import cn.e3.pojo.EasyUIPageBean;
 import cn.e3.pojo.TbItem;
 import cn.e3.pojo.TbItemExample;
-import cn.e3.pojo.TbItemExample.Criteria;
 @Service
 public class ItemServiceImpl implements ItemService{
 	@Autowired
@@ -20,6 +22,28 @@ public class ItemServiceImpl implements ItemService{
 		//根据id查询，设置参数
 		TbItem item = itemMapper.selectByPrimaryKey(itemId);
 		return item;
+	}
+	/**
+	 * 分页查询商品列表
+	 */
+	@Override
+	public EasyUIPageBean itemList(Integer page, Integer rows) {
+	
+		TbItemExample example = new TbItemExample();
+		//设置分页信息
+		PageHelper.startPage(page, rows);
+		//分页查询商品信息
+		List<TbItem> list = itemMapper.selectByExample(example);
+		
+		//创建pageInfo对象获取分页信息
+		PageInfo<TbItem> pageInfo = new PageInfo<TbItem>(list);
+		//创建一个返回值对象pageBean，封装返回结果
+		EasyUIPageBean pageBean = new EasyUIPageBean();
+		//设置总记录数
+		pageBean.setTotle(pageInfo.getTotal());
+		//设置查询分页记录
+		pageBean.setRows(list);
+		return pageBean;
 	}
 
 }
